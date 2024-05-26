7be8af90033e605e87f61e0f57ae594b605e17d0
soyedpork27 <chan4728@kakao.com>
2024-05-26 23:20:42 +0900

    todo_localStorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import './App.css';
 
+import {useState} from 'react';
+
 function App() {
+
+  const [todos , setTodos] = useState(()=>getFromLocalStorage());
+
+  const [todo, setTodo] = useState('');
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos((prev) => {
+      return [...prev,todo];
+    })
+    setTodo('');
+
+    localStorage.setItem("first-todo",JSON.stringify(todos));
+  }
+
+  const deleteAll = () => {
+    localStorage.removeItem("first-todo");
+    setTodos([]);
+  }
+
+
+  function getFromLocalStorage(){
+    if(localStorage.getItem('first-todo')){
+      const result = JSON.parse(localStorage.getItem('first-todo'));
+      return result;
+    }else{
+      return [];
+    }
+  }
+
   return (
     <>
-      리액트
+      <form onSubmit={handleSubmit}>
+        <input value={todo} onChange={handleChange} />
+        <br />
+        <button type='submit'>등록</button>
+      </form>
+
+      <ul>
+        {todos.map((i, idx)=>(<li key={idx}>{i}</li>))}
+      </ul>
+
+      <button onClick={deleteAll}> 모든 메모 삭제 </button>
     </>
   );
 }
